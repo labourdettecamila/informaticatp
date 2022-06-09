0f335622e6765ceee09aa4227940d78117db1963
clarapinasco <cmpinasco24@ucema.edu.ar>
2022-06-09 13:03:17 -0300

    user menu

---
+++ app_pediatrica.py
@@ -0,0 +1,33 @@
+active = True
+
+while active:
+    print("\n")
+    print("App Médica - Opciones")
+    print("\t(1)Actualizar datos")
+    print("\t(2)Ver historial")
+    print("\t(3)Salir")
+
+    option = input("Opción: ")
+
+    if option == "3":
+        active = False
+
+    elif option == "1":
+        persona = []
+        datos = {}
+
+        nombre = input("Nombre del usuario: ")
+        peso = input("Peso: ")
+        altura = input("Altura: ")
+
+        datos['nombre'] = nombre
+        datos['peso'] = peso
+        datos['altura'] = altura
+
+        persona.append(datos)
+
+    elif option == "2":
+        print(f"Historial: {persona}")
+
+    else:
+        print(f"La opción {option} no es válida")
