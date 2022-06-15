1d5a332c5af4d440686f577c204a580b4f01e84b
clarapinasco <cmpinasco24@ucema.edu.ar>
2022-06-15 18:28:36 -0300

    lista de clientes

---
+++ clientes.py
@@ -0,0 +1,36 @@
+import random
+clientes = [
+    {
+        "nombre": "lara",
+        "apellido": "milberg",
+        "fecha de nacimiento": "01/07/2003",
+        "obra social": "osde",
+        "codigo postal": "C1407AOM",
+        "altura": 1.7, "peso": 60,
+        "alergias": None,
+        "id": random.randint(0, 1000000)
+
+    },
+    {
+        "nombre": "lucas",
+        "apellido": "sanchez",
+        "fecha de nacimiento": "27/11/2012",
+        "obra social": "medicus",
+        "codigo postal": "C1232AAC",
+        "altura": 1.5, "peso": 47,
+        "alergias": "acaros",
+        "id": random.randint(0, 1000000)
+
+    },
+    {"nombre": "juana",
+        "apellido": "condomi",
+        "fecha de nacimiento": "05/03/1984",
+        "obra social": "osde",
+        "codigo postal": "C1272AAA",
+        "altura": 1.61, "peso": 73,
+        "alergias": "hongos",
+        "id": random.randint(0, 1000000)
+
+    }
+]
+print(clientes)
