0fd0cd5b3ecb79a47138e3cb1a3426fa85c8daef
MartinaLarrea <102036074+MartinaLarrea@users.noreply.github.com>
2022-06-16 10:04:10 -0300

    Update app_pediatrica.py

--- app_pediatrica.py
+++ app_pediatrica.py
@@ -1,33 +1,33 @@
-active = True
+##active = True
 
-while active:
+#while active:
     print("\n")
     print("App Médica - Opciones")
     print("\t(1)Actualizar datos")
     print("\t(2)Ver historial")
     print("\t(3)Salir")
 
-    option = input("Opción: ")
+#    option = input("Opción: ")
 
-    if option == "3":
+#    if option == "3":
         active = False
 
-    elif option == "1":
-        persona = []
-        datos = {}
+#    elif option == "1":
+#        persona = []
+#        datos = {}
 
-        nombre = input("Nombre del usuario: ")
-        peso = input("Peso: ")
-        altura = input("Altura: ")
+#        nombre = input("Nombre del usuario: ")
+#        peso = input("Peso: ")
+#        altura = input("Altura: ")
 
-        datos['nombre'] = nombre
-        datos['peso'] = peso
-        datos['altura'] = altura
+#        datos['nombre'] = nombre
+#        datos['peso'] = peso
+#        datos['altura'] = altura
 
-        persona.append(datos)
+#        persona.append(datos)
 
-    elif option == "2":
+#    elif option == "2":
         print(f"Historial: {persona}")
 
-    else:
+#   else:
         print(f"La opción {option} no es válida")
