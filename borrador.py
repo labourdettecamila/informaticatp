40e8e095f20630cc900f38b3fa9239da856d7f33
MartinaLarrea <102036074+MartinaLarrea@users.noreply.github.com>
2022-06-16 11:04:00 -0300

    Delete borrador.py

--- borrador.py
+++
@@ -1,7 +0,0 @@
-nombre =
-
-hosp = {"nombre": hospital["NOMBRE"],
-        "calle": hospital["CALLE"],
-        "altura": hospital["ALTURA"],
-        "cod_postal": hospital["COD_POSTAL"]}
-hospitales.append(hosp)
