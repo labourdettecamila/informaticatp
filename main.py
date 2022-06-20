2c012bf5d5d37551b73e8b4b19f07871fa379f37
MartinaLarrea <102036074+MartinaLarrea@users.noreply.github.com>
2022-06-20 14:37:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,29 @@ def get_client(client_id):
     return jsonify({})
 
 
+@app.route("/api/<client_id>", methods=['PUT'])
+def modify_patient(client_id):
+    body = request.json
+
+    contador = 0
+
+    for patient in patients_obj:
+        if patient.id == client_id:
+            
+            paciente_modificado = Patient(body["nombre"], body["apellido"], body["fecha_de_nacimiento"],
+                                                          body["obra_social"], body["codigo_postal"], body["altura"],
+                                                          body["alergias"], body["id"])
+            
+            patients_obj[contador:contador + 1] = paciente_modificado
+            
+            return jsonify(paciente_modificado.serialize())
+
+        else:
+            contador += 1
+    return "No existe un paciente identificado con el ID: " + client_id
+
+
+
 @app.route("/api/delete_patient/<client_id>", methods=['DELETE'])
 def delete_patient(client_id):
     contador = 0
