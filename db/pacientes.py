d84deba9b4e4152f5daae3aba347f930b5a5223a
MartinaLarrea <102036074+MartinaLarrea@users.noreply.github.com>
2022-06-20 16:20:57 -0300

    Update pacientes.py

--- db/pacientes.py
+++ db/pacientes.py
@@ -1,11 +1,13 @@
 from clase import Patient
 
+from clase import Patient, ObraSocial
+
 pacientes_dic = [
     {
         "nombre": "Lara",
         "apellido": "Milberg",
         "fecha de nacimiento": "01/07/2003",
-        "obra social": "osde",
+        "obra social": ObraSocial("osde", "310", "5555555"),
         "codigo postal": "C1407AOM",
         "altura": 1.7, "peso": 60,
         "alergias": None,
@@ -16,7 +18,7 @@ pacientes_dic = [
         "nombre": "Lucas",
         "apellido": "Sanchez",
         "fecha de nacimiento": "27/11/2012",
-        "obra social": "medicus",
+        "obra social": ObraSocial("Galeano", "A1", "10004588"),
         "codigo postal": "C1232AAC",
         "altura": 1.5, "peso": 47,
         "alergias": "acaros",
@@ -27,7 +29,7 @@ pacientes_dic = [
         "nombre": "Juana",
         "apellido": "Condomi",
         "fecha de nacimiento": "05/03/1984",
-        "obra social": "osde",
+        "obra social": ObraSocial("Osde", "210", "4445200658"),
         "codigo postal": "C1272AAA",
         "altura": 1.61, "peso": 73,
         "alergias": "hongos",
@@ -38,7 +40,7 @@ pacientes_dic = [
         "nombre": "Trinidad",
         "apellido": "Azpiri",
         "fecha de nacimiento": "07/04/2003",
-        "obra social": "osde",
+        "obra social": ObraSocial("Swiss Medical", "MS3", "222457885"),
         "codigo postal": "C1407KQF",
         "altura": 1.59, "peso": 53,
         "alergias": "penicilina",
@@ -49,7 +51,7 @@ pacientes_dic = [
         "nombre": "Clara",
         "apellido": "Pinasco",
         "fecha de nacimiento": "12/02/2002",
-        "obra social": "medicus",
+        "obra social": ObraSocial("Swiss Medical", "MS3", "13554788"),
         "codigo postal": "C1427ARN",
         "altura": 1.7, "peso": 56,
         "alergias": None,
@@ -60,7 +62,7 @@ pacientes_dic = [
         "nombre": "Martina",
         "apellido": "Larrea",
         "fecha de nacimiento": "11/11/2012",
-        "obra social": "swiss medical",
+        "obra social": ObraSocial("Galeano", "220", "10078555"),
         "codigo postal": "C1439CNU",
         "altura": 1.55, "peso": 60,
         "alergias": "alergia al polen",
@@ -71,7 +73,7 @@ pacientes_dic = [
         "nombre": "Felipe",
         "apellido": "Canale",
         "fecha de nacimiento": "31/08/2000",
-        "obra social": "swiss medical",
+        "obra social": ObraSocial("Swiss Medical", "Black", "144452445"),
         "codigo postal": "C1284AGA",
         "altura": 1.7, "peso": 87,
         "alergias": "acaros",
@@ -80,6 +82,7 @@ pacientes_dic = [
     }
 ]
 
+print(pacientes_dic)
 
 def load_patients():
     patients_obj = []
@@ -99,5 +102,5 @@ def load_patients():
             )
     return patients_obj
 
-pacientes_objetos = load_patients()
+
 
