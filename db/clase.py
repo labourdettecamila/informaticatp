a3d7ba61825418cc8cf4efac1ff66fc948b06b06
MartinaLarrea <102036074+MartinaLarrea@users.noreply.github.com>
2022-06-20 20:12:25 -0300

    Update clase.py

--- db/clase.py
+++ db/clase.py
@@ -1,13 +1,14 @@
 # creacion de la clase paciente
 class Patient:
 
-    def __init__(self,  nombre, apellido, fecha_de_nacimiento, obra_social, codigo_postal, altura, alergias, id) -> None:
+    def __init__(self,  nombre, apellido, fecha_de_nacimiento, obra_social, codigo_postal, altura, peso, alergias, id) -> None:
         self.nombre = nombre
         self.apellido = apellido
         self.fecha_de_nacimiento = fecha_de_nacimiento
         self.obra_social: ObraSocial = obra_social
         self.codigo_postal = codigo_postal
         self.altura = altura
+        self.peso = peso
         self.alergias = alergias
         self.id = id
 
@@ -19,6 +20,7 @@ class Patient:
             'obra_social': self.obra_social.serialize(),
             'codigo_postal': self.codigo_postal,
             'altura': self.altura,
+            "peso" : self.peso,
             'alergias': self.alergias,
             'id': self.id,
         }
