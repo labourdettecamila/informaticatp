30f375cc2e0b878523edf319f59bf1f6b4b54bdd
MartinaLarrea <102036074+MartinaLarrea@users.noreply.github.com>
2022-06-20 16:20:04 -0300

    Update clase.py

--- db/clase.py
+++ db/clase.py
@@ -4,7 +4,7 @@ class Patient:
         self.nombre = nombre
         self.apellido = apellido
         self.fecha_de_nacimiento = fecha_de_nacimiento
-        self.obra_social = obra_social
+        self.obra_social: ObraSocial = obra_social
         self.codigo_postal = codigo_postal
         self.altura = altura
         self.alergias = alergias
@@ -15,9 +15,25 @@ class Patient:
             'nombre': self.nombre,
             'apellido': self.apellido,
             'nacimiento': self.fecha_de_nacimiento,
-            'obra_social': self.obra_social,
+            'obra_social': self.obra_social.serialize(),
             'codigo_postal': self.codigo_postal,
             'altura': self.altura,
             'alergias': self.alergias,
             'id': self.id,
+        }
+
+
+class ObraSocial(Patient):
+
+    def __init__(self, nombre, plan, numero_tarjeta) -> None:
+        self.nombre = nombre
+        self.plan = plan
+        self.numero_tarjeta = numero_tarjeta
+
+
+    def serialize(self):
+        return {
+            'nombre': self.nombre,
+            'plan': self.plan,
+            'numero_tarjeta': self.numero_tarjeta,
         }
