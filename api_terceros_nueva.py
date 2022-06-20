b3a537aee13648d574855979ec7218782cd631b4
MartinaLarrea <102036074+MartinaLarrea@users.noreply.github.com>
2022-06-20 19:51:12 -0300

    Update api_terceros_nueva.py

--- api_terceros_nueva.py
+++ api_terceros_nueva.py
@@ -1,22 +1,26 @@
 import requests
 url = 'https://cdn.buenosaires.gob.ar/datosabiertos/datasets/salud/hospitales/hospitales.geojson'
 
+
+# guardo en la variable response la informacion de la url
 response = requests.get(url)
 
 hospitales_api = (response.json())
 
-#print(hospitales_api["features"][1]["properties"])
-
+# creo una lista de hospitales 
 hospitales = []
 
+# recorro la lista de hospitales
 for hospital in hospitales_api["features"]:
+    
+    # creo un diccionario con la informacion relevante del hospital que estoy recorriendo
     hosp = {"nombre": hospital['properties']["NOMBRE"],
             "calle": hospital['properties']["CALLE"],
             "altura": hospital['properties']["ALTURA"],
             "cod_postal": hospital['properties']["COD_POSTAL"]}
+    
+    # agrego el diccionario a la nueva lista de hospitales
     hospitales.append(hosp)
-
-print(hospitales)
 
 
 
